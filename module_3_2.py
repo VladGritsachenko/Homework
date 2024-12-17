24e7a49d15fa712432ca418faa677e5770b9e584
VladGritsachenko <vladislavgritsachenko@gmail.com>
2024-12-17 10:25:42 +0300

    Выполненное домашнее задание 2 из 3 модуля

---
+++ module_3_2.py
@@ -0,0 +1,23 @@
+#Задача "Рассылка писем"
+variants = ('.ru', '.net', '.com')
+def send_email(message, recipient, sender = "university.help@gmail.com" ):
+    if not '@' in recipient or not '@' in sender or not recipient.endswith(variants) or not sender.endswith(variants):
+        print('Невозможно отправить письмо с адреса <sender> на адрес <recipient>.')
+        return
+    if recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+        return
+    if sender != "university.help@gmail.com":
+        print('Письмо успешно отправлено с адреса <sender> на адрес <recipient>.')
+        return
+    else:
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>.')
+        return
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
